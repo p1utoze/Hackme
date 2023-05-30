8469d2a25d6ea49bc166ab5fa0bf13bc54f4fec5
p1utoze <1ds21ai001@dsce.edu.in>
2023-05-30 11:51:09 +0530

    Added login.py

--- main.py
+++ main.py
@@ -70,34 +70,6 @@ async def home(request: Request):
     return RedirectResponse("https://hackaventus.com/")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-class LoginForm:
-    def __init__(self, request: Request):
-        self.request: Request = request
-        self.errors: List = []
-        self.username: Optional[str] = None
-        self.password: Optional[str] = None
-
-    async def load_data(self):
-        form = await self.request.form()
-        self.username = form.get(
-            "email"
-        )  # since outh works on username field we are considering email as username
-        self.password = form.get("password")
-
-    async def is_valid(self):
-        if not self.username or not (self.username.__contains__("@")):
-            self.errors.append("Email is required")
-        if not self.password or not len(self.password) >= 4:
-            self.errors.append("A valid password is required")
-        if not self.errors:
-            return True
-        return False
-
-
 @app.get("/barcode_reader", response_class=HTMLResponse)
 async def barcode_reader(request: Request):
     return templates.TemplateResponse('barcode_reader_page.html', {"request": request})
