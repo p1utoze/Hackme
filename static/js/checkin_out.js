80d53ffddfa9e8c69862ca2b878c84ab2eda40d8
Chandrahas B <372002chandrahas@gmail.com>
2023-05-31 13:32:33 +0530

    master check in

--- static/js/checkin_out.js
+++ static/js/checkin_out.js
@@ -6,3 +6,20 @@ function checker()
         return true
     return false
 }
+
+function verifyPassword() {
+    event.preventDefault()
+    let passwordInput = document.getElementById("input").value;
+    const passphrase = "{{ password }}";  // Replace with your actual passphrase
+    console.log(passphrase);
+    console.log(passwordInput);
+    console.log(typeof passwordInput === passphrase)
+    if (passwordInput === passphrase) {
+        // Password matches, submit the form
+        document.forms[0].submit();
+
+    } else {
+        // Password doesn't match, display an error message or take appropriate action
+        alert("Incorrect password");
+    }
+}
