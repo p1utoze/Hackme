e842afe5314f93b3970374b599cfb1000fec8f1a
p1utoze <1ds21ai001@dsce.edu.in>
2023-11-27 00:05:48 +0530

    update: add google auth button as form data

--- auth/google_sso.py
+++ auth/google_sso.py
@@ -2,36 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import RedirectResponse
 from fastapi_sso.sso.google import GoogleSSO
 from starlette.requests import Request
-from dotenv import load_dotenv
-from pathlib import Path
 import os
 import firebase_admin
-from firebase_admin import auth as fb_auth, credentials
-
-
-directory_path = Path(__file__).parent
-env_file_path = directory_path.parent / '.env'
-
-load_dotenv()
-GOOGLE_CLIENT_ID =  os.getenv("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET =  os.getenv("GOOGLE_CLIENT_SECRET")
-HOST = os.getenv("HOSTNAME_URI")
-
-
-firebaseConfig = {
-  "apiKey": "AIzaSyDYt2Mj95NipbFOhBMb3jDsdUoXi7YFFUc",
-  "authDomain": "hackme-60c66.firebaseapp.com",
-  "databaseURL": "https://hackme-60c66-default-rtdb.asia-southeast1.firebasedatabase.app",
-  "projectId": "hackme-60c66",
-  "storageBucket": "hackme-60c66.appspot.com",
-  "messagingSenderId": "505276403628",
-  "appId": "1:505276403628:web:4e292a5a0395c348543409",
-  "measurementId": "G-QR40ZRVFT3"
-}
-firebase_app = firebase_admin.initialize_app(firebaseConfig)
-
-cred = credentials.Certificate('hackme.json')
-fir_app = firebase_admin.initialize_app(cred)
+from firebase_admin import auth as fb_auth
+from utils import GOOGLE_CLIENT_SECRET, GOOGLE_CLIENT_ID, HOST
 
 
 def get_google_sso() -> GoogleSSO:
@@ -71,6 +45,6 @@ async def auth_callback(request: Request, sso=Depends(get_google_sso)):
         response.set_cookie(key="firebase_token", value=user_id_token.decode(), httponly=True, max_age=1800)
         return response
     except firebase_admin._auth_utils.UserNotFoundError:
-        redirect_url = request.url_for('root') + '?x-error=Invalid+credentials'
+        redirect_url = request.url_for('root') + '?x-error=Unauthorized+access'
         return RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND,
-                                headers={"x-error": "Invalid credentials"})
+                                headers={"x-error": "Unauthorized access"})
