8214668dd4505e0138684ca394bc4e6454fbba25
p1utoze <1ds21ai001@dsce.edu.in>
2023-11-28 23:07:25 +0530

    feat: add path variables for the app

---
+++ app/settings.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PARENT_DIR = Path(__file__).parent
+static_dir = PARENT_DIR / "static"
+data_path = PARENT_DIR / "data" / "Final_List.csv"
+template_dir = PARENT_DIR / "templates"
