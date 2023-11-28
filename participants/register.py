bb5f362acacb2396294bae22b18185adf71b1cf6
p1utoze <1ds21ai001@dsce.edu.in>
2023-11-28 19:29:10 +0530

    refactor: move check_particpants function from main.py to register.py

--- participants/register.py
+++ participants/register.py
@@ -5,8 +5,42 @@ from api_globals import g, GlobalsMiddleware
 from fastapi.templating import Jinja2Templates
 from auth.utils import db
 from google.cloud.firestore_v1.base_query import FieldFilter
+
 router = APIRouter(prefix="/v1/teams")
 templates = Jinja2Templates(directory="../templates")
+
+
+def check_participant(uid=None, is_admin=None):
+    if not uid:
+        # The participant is not registered. Wrong QRCODE
+        return 602
+    uid = uid.strip()
+
+    print(f'"{uid}"')
+    # Registered participants
+    s = uid in g.df['UID'].tolist()
+    if s:
+        participants = "participants"
+        cursor = db.collection(participants)
+        query = cursor.where(filter=FieldFilter("UID", "==", uid)).get()
+
+        try:
+            data = query[0].to_dict()
+            print(f'{query[0].id} => {data}')
+        except IndexError:
+            data = {}
+            print("No data found")
+
+        # Check if uid in firestore
+        if data:
+            print("ALREADY REGISTERED USER. REDIRECTING TO CHECKIN...")
+            return 600
+        print("NOT REGISTERED. REDIRECTING TO REGISTRATION...")
+        return 601
+
+    else:
+        # Invalid
+        return "INVALID UID", {'Error': 'INVALID UID FOUND'},
 
 
 def fetch_user_status(uid, participants, entry_time):
